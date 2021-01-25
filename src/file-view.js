ff949d487ed1e32e53a5883d4f14524ed20c8114
abhishek <abhi1999@gmail.com>
2021-01-24 22:25:21 -0500

    Added modal view to see details

---
+++ src/file-view.js
@@ -0,0 +1,56 @@
+import React from "react"
+import AceEditor from 'react-ace';
+
+
+class FileDetails extends React.Component {
+    constructor(props) {
+      super(props);
+      this.editorRe={}
+
+      this.state = {
+      }
+    }
+    render() {
+        if(this.props.FileDetails && this.props.FileDetails.length>0){
+            return (<div>Please select one file to preview, or click on "Download" button to download the selected files</div>)
+        }
+        const formatedXML =
+`<a>
+    <b>dd</b>
+</a>`;
+        debugger;
+        return(
+        <div className="file-details">
+            <AceEditor
+                            ref={(ref) => { this.editorRef = ref;
+                                                if(ref && ref.editor && ref.editor.commands && ref.editor.commands.byName && ref.editor.commands.byName.find && ref.editor.commands.byName.find.exec) {
+                                                    ref.editor.$blockScrolling='Infinity';
+                                                    ref.editor.commands.byName.find.exec(ref.editor) 
+                                                }
+                                            }}
+                            mode="xml"
+                            editorProps={{$blockScrolling: Infinity}}
+                            theme="github"
+                            name="xmlEditor"
+                            showPrintMargin={true}
+                            showGutter={true}
+                            highlightActiveLine={true}
+                            value={formatedXML}
+                            height={'100%'}
+                            width={'100%'}
+                            setOptions={{
+                                enableBasicAutocompletion: false,
+                                enableLiveAutocompletion: false,
+                                enableSnippets: false,
+                                showLineNumbers: true,
+                                tabSize: 2,
+                            }}
+                            readOnly={false}
+                            />
+        </div>
+        );
+    }
+
+}
+
+export default FileDetails;
--- src/syncfusion-file-manager.js
+++ src/syncfusion-file-manager.js
@@ -10,6 +10,8 @@ import '../node_modules/@syncfusion/ej2-layouts/styles/material.css';
 import '../node_modules/@syncfusion/ej2-grids/styles/material.css';
 import "../node_modules/@syncfusion/ej2-react-filemanager/styles/material.css";
 import { DetailsView, FileManagerComponent, NavigationPane, Toolbar, Inject  } from '@syncfusion/ej2-react-filemanager';
+import FileDetails from "./file-view";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 // FileManagerComponent.Inject(DetailsView, NavigationPane, Toolbar);
 
@@ -21,23 +23,60 @@ class Control2 extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
+        isModalOpen:false,
         hostUrl1:"http://localhost:8090/",
+        fileDetails:[],
         hostUrl:REACT_APP_SYNC_API_HOST//"https://file-manager-node-server.herokuapp.com/"//"http://localhost:8090/"// "https://ej2-aspcore-service.azurewebsites.net/"
       }
     }
-    
+    menuOpen(args){
+      for(let i = 0; i<args.items.length; i++) {
+        if((args.items[i].id === this.element.id +'_cm_custom') ||(args.items[i].id === this.element.id +'_cm_view contents')) {
+            args.items[i].iconCss= 'e-icons e-fe-open';
+        }
+      }
+    }
+    menuClick=(args)=>{
+      if ((args.item.text === 'Custom')||(args.item.text === 'View Contents')) {
+        this.setState({isModalOpen:true, fileDetails:args.fileDetails})
+      } 
+    }
+    toggle=()=>{
+      this.setState({isModalOpen:!this.state.isModalOpen})
+    }
     render() {
-        const {hostUrl}= this.state
+        const {hostUrl, isModalOpen, fileDetails}= this.state
+        const className ="file-details-modal";
         return(
         <div className="control-section">
-            <FileManagerComponent  ref={s => (this.fileObj = s)} id="file" allowDragAndDrop={true}  view="LargeIcons" ajaxSettings={{
-                downloadUrl: hostUrl + 'Download',
-                getImageUrl: hostUrl + "GetImage",
-                uploadUrl: hostUrl + 'Upload',
-                url: hostUrl
-             }}>
+            <FileManagerComponent  ref={s => (this.fileObj = s)} id="file" 
+                allowDragAndDrop={true}  
+                view="LargeIcons" 
+                ajaxSettings={{
+                  downloadUrl: hostUrl + 'Download',
+                  // getImageUrl: hostUrl + "GetImage",
+                  uploadUrl: hostUrl + 'Upload',
+                  url: hostUrl
+                }}
+                contextMenuSettings= {{
+                  file: ["View Contents", "|", "Cut","Copy","|", "Delete", "Download", "Rename", "|", "Details"],
+                  visible: true
+                }}
+                menuOpen={this.menuOpen}
+                menuClick={this.menuClick}
+             >
             <Inject services={[NavigationPane, DetailsView, Toolbar]}/>
           </FileManagerComponent>
+          <Modal isOpen={isModalOpen} toggle={this.toggle} className={className}>
+            <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+            <ModalBody>
+                <FileDetails fileDetails={fileDetails}/>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+              <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
         </div>
         );
     }
